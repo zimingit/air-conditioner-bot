a2a4d2b6c102a7e7db048556d4c161a648ef5c07
Алексей Зимин <alexey.zimin@rightstep.ru>
2023-08-13 18:16:49 +0300

    FEAT сборка, базовый путь

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/climate-bot', 
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src')
